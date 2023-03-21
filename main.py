c9f54b7f1dc7ca0b0559b4bf1f77766f74fc30aa
Johann Schmid <50572495+Johann-Schmid@users.noreply.github.com>
2023-03-21 12:17:27 +0100

    Start of project

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def printHexDualOct(inputNumber):
+    print(f'Bin {bin(inputNumber)}');
+    print(f'Oct {oct(inputNumber)}');
+    print(f'Hex {hex(inputNumber)}');
+
+def saveFile(numberInput):
+    filename = "binary_file.bin"  # The filename to save the binary data to
+    num = int(numberInput)
+    # Convert the string to binary data
+    binary_data = num.to_bytes(1, byteorder='big')
+
+    # Write the binary data to a file
+    with open(filename, "wb") as file:
+        file.write(binary_data)
+
+    print(f"The number '{numberInput}' has been saved as binary data to '{filename}'.")
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    number = input('Enter a number: ');
+    #printHexDualOct(int(number));
+    saveFile(number);
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
